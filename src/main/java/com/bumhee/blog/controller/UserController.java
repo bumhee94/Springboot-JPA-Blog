95129de86f9c33a970e9c8abe7e7943af05f66fb
이범희 <bhlee94@naver.com>
2022-03-30 22:02:16 +0900

    카카오 로그인 api 개발

--- src/main/java/com/bumhee/blog/controller/UserController.java
+++ src/main/java/com/bumhee/blog/controller/UserController.java
@@ -1,8 +1,16 @@
 package com.bumhee.blog.controller;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 import com.bumhee.blog.config.auth.PrincipalDetail;
 
@@ -25,5 +33,30 @@ public class UserController {
 	public String updateForm(@AuthenticationPrincipal PrincipalDetail principal)
 	{
 		return "user/updateForm";
+	}
+	
+	@GetMapping("/auth/kakao/callback")
+	public @ResponseBody String kakaoCallback(String code)
+	{
+		RestTemplate rt = new RestTemplate();
+		HttpHeaders headers = new  HttpHeaders();
+		headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+		
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("grant_type", "authorization_code");
+		params.add("client_id", "79ba785d98f8e559e33d06a3daac8ebf");
+		params.add("redirect_uri", "http://localhost:8000/auth/kakao/callback");
+		params.add("code", code);
+		
+		HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
+		
+		ResponseEntity<String> reponse = rt.exchange(
+				"https://kauth.kakao.com/oauth/token",
+				HttpMethod.POST,
+				kakaoTokenRequest,
+				String.class
+				);
+		
+		return "카카오 인증 완료 코드값 : " + reponse;
 	}
 }
